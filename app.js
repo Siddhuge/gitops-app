768af76b8638a235230353fc6a1e71acb40371c1
Siddhant Huge <94670258+Siddhuge@users.noreply.github.com>
2026-04-06 17:35:58 +0530

    Update app version message to version 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("GitOps App Version 1 🚀");
+  res.send("GitOps App Version 2 is Live using GitOps 🚀");
 });
 
 app.listen(3000, () => console.log("Running on port 3000"));
